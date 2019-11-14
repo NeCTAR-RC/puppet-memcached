1d3f52a9bad4ac2da7b1d8ac93bafe6929747595
Jake Yip <jake.yip@unimelb.edu.au>
2019-11-14 13:59:59 +1100

    Update python-memcached to stein oslo::cache format
    To prevent duplicate declaration errors
    
    Change-Id: I7cbad4b2ebc6f5c585a5650b777027714b890182

--- manifests/python.pp
+++ manifests/python.pp
@@ -1,8 +1,9 @@
 # Installs python memcache bindings
 class memcached::python {
+  include ::oslo::params
   
   ensure_resources('package', { 'python-memcache' => {
-    name   => 'python-memcache',
+    name   => $::oslo::params::python_memcache_package_name,
     tag    => ['openstack'],
     }})
 
