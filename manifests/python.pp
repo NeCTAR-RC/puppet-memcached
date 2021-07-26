95f921bf8435174f089b77c205964cafe85a88fa
Sam Morrison <sorrison@gmail.com>
2021-07-26 14:59:38 +1000

    Change python-memcache declaration to match oslo.cache
    Change-Id: Ic851f929a65b4d9cef7f8d0fed69d1a12eb465fb

--- manifests/python.pp
+++ manifests/python.pp
@@ -1,10 +1,10 @@
 # Installs python memcache bindings
 class memcached::python {
   include ::oslo::params
-  
-  ensure_resources('package', { 'python-memcache' => {
+
+  ensure_packages('python-memcache', {
     name   => $::oslo::params::python_memcache_package_name,
     tag    => ['openstack'],
-    }})
+    })
 
 }
